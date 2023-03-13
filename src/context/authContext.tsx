2cca3ffc12752bb40f9d824b05d44e9164d23df3
Ho Won Cheng <howon.cheng@giantmachines.com>
2023-03-13 12:40:58 -0400

    Remove auth context functionality

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -1,13 +1,6 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext } from "react";
 import jwt_decode from "jwt-decode";
-import { login as loginApi, signUp as signUpApi } from "../utils/api";
 import { LoginBody, SignUpBody, User } from "../utils/apiTypes";
-import {
-  clearAccessToken,
-  getAccessToken,
-  isAccessTokenExpired,
-  setAccessToken,
-} from "../utils/accessTokenStorage";
 import { noop } from "../utils/noop";
 
 export const ACCESS_TOKEN_KEY = "ACCESS_TOKEN_KEY";
@@ -28,41 +21,17 @@ const AuthContext = createContext<{
 }>(defaultContext);
 
 function AuthProvider(props: { children: React.ReactNode }) {
-  const [user, setUser] = useState<User | null>(defaultContext.user);
-  const signUp = (user: SignUpBody) => {
-    return signUpApi(user);
-  };
-  const logout = () => {
-    clearAccessToken();
-    setUser(null);
+  const user = null as User | null;
+  const signUp = async (user: SignUpBody) => {
+    console.log("TODO: ADD SIGNUP FUNCTIONALITY");
+    return { isSuccess: false };
   };
   const login = async (credentials: LoginBody) => {
-    try {
-      const jwt = await loginApi(credentials);
-      const accessToken = jwt["access_token"];
-      if (!accessToken) {
-        throw new Error("No access token");
-      }
-      setAccessToken(accessToken);
-      const userResponse = jwt_decode<User>(accessToken);
-      setUser(userResponse);
-      return userResponse;
-    } catch (err) {
-      return null;
-    }
+    console.log("TODO: ADD LOGIN FUNCTIONALITY");
   };
-
-  useEffect(() => {
-    const storedAccessToken = getAccessToken();
-    if (storedAccessToken) {
-      const storedUser = jwt_decode<User>(storedAccessToken);
-      if (isAccessTokenExpired(storedUser.exp)) {
-        clearAccessToken();
-      } else {
-        setUser(storedUser);
-      }
-    }
-  }, []);
+  const logout = () => {
+    console.log("TODO: ADD LOGOUT FUNCTIONALITY");
+  };
 
   return (
     <AuthContext.Provider value={{ user, login, signUp, logout, isLoggedIn: Boolean(user) }}>
